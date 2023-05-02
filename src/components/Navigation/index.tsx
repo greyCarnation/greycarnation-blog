8e531fea398a40a27d5573c11b238de63140873f
devUserContact <developer.user.contact@protonmail.com>
2023-05-01 22:48:49 -0700

    edited `Navigation` styles

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -4,26 +4,28 @@ import styles from '../../styles/Main.module.scss'
 
 const Navigation = () => {
   return (
-    <div className={styles.navbarContainer}>
-      <div className={styles.navItem}>
-        <Link href='/music'>
-          <p className={styles.navText}>music</p>
-        </Link>
-      </div>
-      <div className={styles.navItem}>
-        <Link href='/socialmedia'>
-          <p className={styles.navText}>socialMedia</p>
-        </Link>
-      </div>
-      <div className={styles.navItem}>
-        <Link href='/about'>
-          <p className={styles.navText}>about</p>
-        </Link>
-      </div>
-      <div className={styles.navItem}>
-        <Link href='/contact'>
-          <p className={styles.navText}>contact</p>
-        </Link>
+    <div className={styles.containerNavbar}>
+      <div className={styles.gridNavbar}>
+        <div className={styles.itemNavbar}>
+          <Link href='/music'>
+            <p className={styles.textNav}>music</p>
+          </Link>
+        </div>
+        <div className={styles.itemNavbar}>
+          <Link href='/about'>
+            <p className={styles.textNav}>about</p>
+          </Link>
+        </div>
+        <div className={styles.itemNavbar}>
+          <Link href='/socialmedia'>
+            <p className={styles.textNav}>socialMedia</p>
+          </Link>
+        </div>
+        <div className={styles.itemNavbar}>
+          <Link href='/contact'>
+            <p className={styles.textNav}>contact</p>
+          </Link>
+        </div>
       </div>
     </div>
   )
