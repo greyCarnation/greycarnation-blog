86b5441231a4582c9f713e2859deeae2de89b147
devUserContact <developer.user.contact@protonmail.com>
2023-05-05 15:20:39 -0700

    edited `AboutText` asset

--- src/assets/AboutText/index.ts
+++ src/assets/AboutText/index.ts
@@ -1,9 +1,9 @@
 export const AboutText: string=`# __about___
 
 _greyCarnation_ is a multi-media art project created by maxwell wine that 
-focuses on the human response to the consumption of art. 
+focuses on the human response to art consumption. 
 
-maxwell uses greyCarnation as a vehicle for presenting illusions throughout 
-art's history; creating a unique experience for the consumer. 
+maxwell uses greyCarnation as a vehicle for presenting aesthetic, illusion, 
+and emotion.
 `
 
