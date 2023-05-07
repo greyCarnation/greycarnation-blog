797fb7d7c0a4059e46450649adaacfc144553f4b
devUserContact <developer.user.contact@protonmail.com>
2023-05-07 00:14:30 -0700

    added `SocialMediaObject` asset

---
+++ src/assets/SocialMediaObject/index.ts
@@ -0,0 +1,17 @@
+export interface SocialMediaObject {
+  platform: string
+  link: string | string[]
+  imageURL: string | string[]
+}
+export const Projects: SocialMediaObject[] = [
+  {
+    platform: 'twitter',
+    link: 'https://twitter.com/greyCarnation_',
+    imageURL: '',
+  },
+  {
+    platform: 'instagram',
+    link: 'https://www.instagram.com/greycarnation_/',
+    imageURL: '',
+  },
+]
